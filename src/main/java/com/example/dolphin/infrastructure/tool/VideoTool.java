df32e3b83dcf374fdb88731441518e639b6498d9
王景阳 <3154834560@qq.com>
2022-11-24 14:51:45 +0800

    减小jar包

--- src/main/java/com/example/dolphin/infrastructure/tool/VideoTool.java
+++ src/main/java/com/example/dolphin/infrastructure/tool/VideoTool.java
@@ -1,10 +1,11 @@
 package com.example.dolphin.infrastructure.tool;
 
+
 import com.example.dolphin.infrastructure.consts.StringPool;
 import lombok.extern.slf4j.Slf4j;
-import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -12,11 +13,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
- * org.bytedeco.javacv 0.8 版本
- * 使用此方法获取视频第一帧方法
- * 此方法只需要org.bytedeco.javacv 0.8 jar较小
- * 但是速度吗
- *
  * @author 王景阳
  * @date 2022/11/24 9:37
  */
@@ -29,35 +25,22 @@ public class VideoTool {
      * @param videoPath 视频路径，可以为 "https://vt1.doubanio.com/202101120940/a3e7ae32c21341710eaceba2d2e56039/view/movie/M/402680931.mp4"
      * @param imagePath 生成的图片保存路径
      * @param imageType 生成的图片类型
-     * @param angele    生成的图片旋转角度
+     * @param angele     生成的图片旋转角度
      */
     public static void getVideoFirstImg(String videoPath, String imagePath, String imageType, int angele) throws Exception {
         String videoName = videoPath.substring(videoPath.lastIndexOf(StringPool.SLASH) + 1);
         log.info("开始获取[ " + videoName + " ]视频第一帧");
+        Frame frame = null;
         //构造器支持InputStream，可以直接传MultipartFile.getInputStream()
         FFmpegFrameGrabber fFmpegFrameGrabber = new FFmpegFrameGrabber(videoPath);
         //开始播放
         fFmpegFrameGrabber.start();
         //获取视频总帧数
-        int lengthInFrames = fFmpegFrameGrabber.getLengthInFrames();
+        int ftp = fFmpegFrameGrabber.getLengthInFrames();
         //指定第几帧
-        int frameNum = 0;
-        fFmpegFrameGrabber.setFrameNumber(frameNum);
+        fFmpegFrameGrabber.setFrameNumber(0);
         //获取指定第几帧的图片
-        Frame f = fFmpegFrameGrabber.grabFrame();
-        Frame frame = f;
-        while (frameNum < lengthInFrames) {
-            // 过滤前10帧，避免出现全黑的图片，依自己情况而定
-            f = fFmpegFrameGrabber.grabFrame();
-            if (f != null && f.image != null) {
-                frame = f;
-            }
-            if (frameNum >= 10) {
-                break;
-            }
-            frameNum++;
-            fFmpegFrameGrabber.setFrameNumber(frameNum);
-        }
+        frame = fFmpegFrameGrabber.grabImage();
         //文件绝对路径+名字
         File outPut = new File(imagePath);
         ImageIO.write(frameToBufferedImage(frame, angele), imageType, outPut);
@@ -65,41 +48,27 @@ public class VideoTool {
         fFmpegFrameGrabber.stop();
         log.info("获取视频[ " + videoName + " ]第一帧成功");
     }
-
     /**
      * 获取视频最后一帧
      *
      * @param videoPath 视频路径，可以为 "https://vt1.doubanio.com/202101120940/a3e7ae32c21341710eaceba2d2e56039/view/movie/M/402680931.mp4"
      * @param imagePath 生成的图片保存路径
      * @param imageType 生成的图片类型
-     * @param angele    生成的图片旋转角度
+     * @param angele     生成的图片旋转角度
      */
     public static void getVideoLastImg(String videoPath, String imagePath, String imageType, int angele) throws Exception {
         String videoName = videoPath.substring(videoPath.lastIndexOf(StringPool.SLASH) + 1);
         log.info("开始获取视频[ " + videoName + " ]最后一帧");
-        FFmpegFrameGrabber fFmpegFrameGrabber = new FFmpegFrameGrabber(new File(videoPath));
+        Frame frame = null;
+        FFmpegFrameGrabber fFmpegFrameGrabber = new FFmpegFrameGrabber(videoPath);
         //开始播放
         fFmpegFrameGrabber.start();
         //获取视频总帧数
-        int lengthInFrames = fFmpegFrameGrabber.getLengthInFrames();
+        int ftp = fFmpegFrameGrabber.getLengthInFrames();
         //指定第几帧
-        int frameNum = lengthInFrames - 1;
-        fFmpegFrameGrabber.setFrameNumber(frameNum);
+        fFmpegFrameGrabber.setFrameNumber(ftp - 5);
         //获取指定第几帧的图片
-        Frame f = fFmpegFrameGrabber.grabFrame();
-        Frame frame = f;
-        while (frameNum >= 0) {
-            // 过滤后20帧，避免出现全黑的图片，依自己情况而定
-            f = fFmpegFrameGrabber.grabFrame();
-            if (f != null && f.image != null) {
-                frame = f;
-            }
-            if (lengthInFrames - frameNum >= 20) {
-                break;
-            }
-            frameNum--;
-            fFmpegFrameGrabber.setFrameNumber(frameNum);
-        }
+        frame = fFmpegFrameGrabber.grabImage();
         File outPut = new File(imagePath);
         ImageIO.write(frameToBufferedImage(frame, angele), imageType, outPut);
         fFmpegFrameGrabber.flush();
@@ -138,13 +107,7 @@ public class VideoTool {
     }
 
     private static BufferedImage frameToBufferedImage(Frame frame, int angele) {
-        opencv_core.IplImage img = frame.image;
-        int width = img.width();
-        int height = img.height();
-        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-        bi.getGraphics().drawImage(frame.image.getBufferedImage().getScaledInstance(width, height, Image.SCALE_SMOOTH),
-                0, 0, null);
-        return rotatePhonePhoto(bi, angele);
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+        return rotatePhonePhoto(converter.getBufferedImage(frame), angele);
     }
-
 }
