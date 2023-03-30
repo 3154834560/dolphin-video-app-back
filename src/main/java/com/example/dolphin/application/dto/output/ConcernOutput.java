b0b3cef7ed6ad33d85ca4971030c92c426dbf079
王景阳 <3154834560@qq.com>
2023-03-30 17:16:06 +0800

    清除ConcernOutput多余字段

--- src/main/java/com/example/dolphin/application/dto/output/ConcernOutput.java
+++ src/main/java/com/example/dolphin/application/dto/output/ConcernOutput.java
@@ -10,23 +10,13 @@ import lombok.Data;
 @Data
 public class ConcernOutput {
     /**
-     *  用户名
+     * 用户名
      */
     private String userName;
-    /**
-     *  头像名称-带后缀
-     */
-    private String headPortraitName;
-    /**
-     *  昵称
-     */
-    private String nick;
 
     public static ConcernOutput of(Concern concern) {
         ConcernOutput output = new ConcernOutput();
         output.setUserName(concern.getConcernedUser().getUserName());
-        output.setHeadPortraitName(concern.getConcernedUser().getHeadPortraitName());
-        output.setNick(concern.getConcernedUser().getNick());
         return output;
     }
 
